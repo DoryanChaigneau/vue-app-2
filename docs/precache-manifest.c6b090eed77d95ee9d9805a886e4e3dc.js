b92566b6d0ea52d2c72dbf8999ec7548c312a0cd
Doryan <doryan.chaigneau@hotmail.fr>
2021-02-02 21:59:29 +0100

    Second commit

---
+++ docs/precache-manifest.c6b090eed77d95ee9d9805a886e4e3dc.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "5f281d3a571aeb3fadb9",
+    "url": "/vue-app-2/css/app.8d23bc70.css"
+  },
+  {
+    "revision": "82b9c7a5a3f405032b1db71a25f67021",
+    "url": "/vue-app-2/img/logo.82b9c7a5.png"
+  },
+  {
+    "revision": "8abf17dbc867d5a689fbae5abb4f0730",
+    "url": "/vue-app-2/index.html"
+  },
+  {
+    "revision": "b4532da3bc9135462c95",
+    "url": "/vue-app-2/js/about.548553b6.js"
+  },
+  {
+    "revision": "5f281d3a571aeb3fadb9",
+    "url": "/vue-app-2/js/app.14c3b7d3.js"
+  },
+  {
+    "revision": "ba2a6fda9ddaaddeab4b",
+    "url": "/vue-app-2/js/chunk-vendors.f449932c.js"
+  },
+  {
+    "revision": "a20c75afa1f0a984f0883b71519b2755",
+    "url": "/vue-app-2/manifest.json"
+  },
+  {
+    "revision": "b6216d61c03e6ce0c9aea6ca7808f7ca",
+    "url": "/vue-app-2/robots.txt"
+  }
+]);
