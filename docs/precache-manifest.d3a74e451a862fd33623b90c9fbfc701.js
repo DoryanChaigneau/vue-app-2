64225ece7ab5d2986f5d069b9a0cf0193ea202bd
Doryan <doryan.chaigneau@hotmail.fr>
2021-02-03 10:44:20 +0100

    Build

---
+++ docs/precache-manifest.d3a74e451a862fd33623b90c9fbfc701.js
@@ -0,0 +1,42 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "5e33c695ab82961387b2",
+    "url": "/vue-app-2/css/Recherche.b35f6d94.css"
+  },
+  {
+    "revision": "2679da6f674302dfb60f",
+    "url": "/vue-app-2/css/app.5c9713c3.css"
+  },
+  {
+    "revision": "752e9458627bcd9237efa4798664c50f",
+    "url": "/vue-app-2/img/nature.752e9458.jpg"
+  },
+  {
+    "revision": "fa6b4de135b142df3989775dad9e99ba",
+    "url": "/vue-app-2/index.html"
+  },
+  {
+    "revision": "7049fb26343daf0796c6",
+    "url": "/vue-app-2/js/Favoris.ad097888.js"
+  },
+  {
+    "revision": "5e33c695ab82961387b2",
+    "url": "/vue-app-2/js/Recherche.6e2dcf57.js"
+  },
+  {
+    "revision": "2679da6f674302dfb60f",
+    "url": "/vue-app-2/js/app.cefe5910.js"
+  },
+  {
+    "revision": "ba2a6fda9ddaaddeab4b",
+    "url": "/vue-app-2/js/chunk-vendors.f449932c.js"
+  },
+  {
+    "revision": "a20c75afa1f0a984f0883b71519b2755",
+    "url": "/vue-app-2/manifest.json"
+  },
+  {
+    "revision": "b6216d61c03e6ce0c9aea6ca7808f7ca",
+    "url": "/vue-app-2/robots.txt"
+  }
+]);
